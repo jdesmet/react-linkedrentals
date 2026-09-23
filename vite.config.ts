1d2660951669cd48df012bb7b3c44385a3a0d6d4
Jo Desmet <jo_desmet@yahoo.com>
2026-09-22 18:38:22 -0700

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import { cloudflare } from "@cloudflare/vite-plugin";
-import react from '@vitejs/plugin-react' // Change or remove depending on your framework (vue, svelte, etc.)
+import { defineConfig } from "vite-plus";
+import { nitro } from "nitro/vite"; // Change or remove depending on your framework (vue, svelte, etc.)
+import react from "@vitejs/plugin-react"; // Change or remove depending on your framework (vue, svelte, etc.)
 
 export default defineConfig({
-  plugins: [react(),cloudflare()],
+  plugins: [react(), nitro()],
   server: {
     headers: {
-      'Content-Security-Policy': "frame-ancestors 'self' https://linked-rentals.com https://*.linked-rentals.com",
+      "Content-Security-Policy":
+        "frame-ancestors 'self' https://linked-rentals.com https://*.linked-rentals.com",
     },
   },
-})
-;
-
+  staged: {
+    ".{ts,tsx,js,jsx,vue}": ["eslint --fix", "prettier --write"],
+    ".{css,scss,sass,less,styl}": ["stylelint --fix", "prettier --write"],
+    "*.{json,md,yml,yaml}": ["prettier --write"],
+  },
+});
